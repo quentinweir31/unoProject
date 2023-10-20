6bfd354a5805c28ba68c088637783b794dd46523
quentinweir31 <146482345+quentinweir31@users.noreply.github.com>
2023-10-20 14:12:25 -0400

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -1,14 +1,3 @@
 public class Card {
-    private String colour;
-    private int value;
-    private String type;
 
-    public Card(String colour, int value, String type) {
-        this.value = value;
-        this.type = type;
-    }
-
-    public String toString() {
-        return colour + " " + value + " with type " + type;
-    }
 }//this is a test
