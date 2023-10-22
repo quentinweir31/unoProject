428938b24484114132a9c43308997173e31039b3
quentinweir31 <146482345+quentinweir31@users.noreply.github.com>
2023-10-22 14:31:34 -0400

    Update Deck.java

--- src/Deck.java
+++ src/Deck.java
@@ -1,1 +1,55 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+public class Deck {
+    private ArrayList<Card> deck;
+
+    public Deck() {
+        deck = new ArrayList<Card>();
+        createDeck();
+    }
+
+    private void createDeck() {
+        String[] allColours = {"Red", "Blue", "Yellow", "Green"};
+        //These are all the types of cards except for numbered, along with the amount per colour
+        HashMap<String, Integer> allTypes = new HashMap<>();
+        allTypes.put("Draw", 2);
+        allTypes.put("Skip", 2);
+        allTypes.put("Reverse", 2);
+        allTypes.put("Wild", 4);
+        allTypes.put("Wild Draw", 4);
+        allTypes.put("Flip", 2);
+        // loop through each colour
+        for (int i = 0; i < 4; i++) {
+            // adding numbers 1 through 9 for each colour
+            for (int j = 1; j < 10; j++) {
+                String value = "" + j;
+                Card card = new Card(allColours[i], value);
+                deck.add(card);
+                deck.add(card); //we have to add two of each number per colour
+            }
+            //adding draw, skip, reverse, and flip
+            for (String type : allTypes.keySet()) {
+                int maxOccurence = allTypes.get(type);
+                if (!(type.equals("Wild Draw") || type.equals("Wild"))) {
+                    for (int h = 0; h < maxOccurence; h++) {
+                        Card card = new Card(allColours[i], type);
+                        deck.add(card);
+                    }
+                }
+            }
+            // adding wild and wild draw
+            deck.add(new Card("Wild"));
+            deck.add(new Card("Wild", "Wild Draw"));
+        }
+        //shuffle the deck
+        Collections.shuffle(deck);
+    }
+    public void printDeck() {
+        for (Card card : deck) {
+            System.out.println(card);
+        }
+    }
+}
 
