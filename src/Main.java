0813ae32a6fd868d6094493fa145585c99cb792c
quentinweir31 <146482345+quentinweir31@users.noreply.github.com>
2023-10-22 14:32:26 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // Test the Card class
-
-        // Create cards using shorthand notation
-        Card card1 = new Card("TD"); // TEN of DIAMONDS
-        Card card2 = new Card("JS"); // JACK of SPADES
-        Card card3 = new Card("QC"); // QUEEN of CLUBS
-
-        // Display card information
-        System.out.println("Card 1: " + card1.getRank() + " of " + card1.getSuit());
-        System.out.println("Card 2: " + card2.getRank() + " of " + card2.getSuit());
-        System.out.println("Card 3: " + card3.getRank() + " of " + card3.getSuit());
+        Deck deck = new Deck();
+        deck.printDeck();
     }
 }
