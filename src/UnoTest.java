e4033086c57dffd24efff1720dfd4dbbe2ee604b
JawadMohammed <jayzfire01@gmail.com>
2023-10-22 20:54:06 -0400

    Update UnoTest.java
    Added Skeletons for incoming tests

--- src/UnoTest.java
+++ src/UnoTest.java
@@ -7,7 +7,7 @@ public class UnoTest {
      */
     public UnoTest() {
     }
-
+    
     /**
      * This method is executed once before all the test methods.
      * It sets up the initial state of the test environment.
@@ -16,6 +16,82 @@ public class UnoTest {
     private static void setUp() {
     }
 
+    //Tests pertaining to Player 
+    /**
+     * Test Case to verify player creation via the consructor 
+     */
+    @Test
+    public void test_PlayerConstructor() {
+    }
+
+    
+    /**
+     * Test Case to verify if hand actions such as add/ remove and draw card work
+     */
+    @Test
+    public void test_PlayerHandActions() {
+        //Testing add to hand, remove from hand/ draw car
+        //by creatign a hand, card and deck, and calling the add/remove draw methods
+    }
+
+
+    /**
+     * Test Case to verify player can draw a card from the deck
+     */
+    @Test
+    public void test_PlayerDrawfromDeck() {
+        //create a deck, a use an empty hand and draw a card into it
+    }
+
+    /**
+     * Test Case to verify the display Hand method works appropriralety 
+     */
+    @Test
+    public void test_PlayerDisplayHand() {
+        //create a player with a hand and display it, assert if the values are correct
+    }
+    /**
+     * Test Case to simulate the player taking a turn 
+     */
+    @Test
+    public void test_PlayerTakeTurn() {
+        //simulate play
+    }
+
+     /**
+     * Test Case to verify if the card constrcutor creates regular cards 
+     */
+    @Test
+    public void test_CardConstructor_card() {
+    }
+    
+     /**
+     * Test Case to verify if the card constrcutor creates wild cards 
+     */
+    @Test
+    public void test_CardConstructor_wild() {
+    }
+
+     /**
+     * Test Case to verify the creation of a deck
+     */
+    @Test
+    public void test_DeckConstructor() {
+    }
+    
+     /**
+     * Test Case to verify the DeckShuffle method shuffles the deck
+     */
+    @Test
+    public void test_DeckShuffle() {
+    }
+
+    /**
+     * Test Case to verify if the printDeck method prints the deck correctly
+     */
+    @Test
+    public void test_DeckPrintDeck() {
+    }
 
     /**
      * This method is executed once after all the test methods.
@@ -26,14 +102,5 @@ public class UnoTest {
     private static void tearDown() {
         System.out.print("All tests are done");
     }
-
-    /**
-     * Test case to verify the default constructor of the Heater class.
-     * It checks if the default temperature is set to 15 degrees.
-     */
-    @Test
-    public void test_DefaultConstructor() {
-    }
-    
 
 }
