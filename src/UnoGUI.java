8f0edee1272e663c7ea1041d8a0d268404f4f69f
jonashall8 <121309342+jonashall8@users.noreply.github.com>
2023-11-08 14:36:12 -0500

    Update UnoGUI.java

--- src/UnoGUI.java
+++ src/UnoGUI.java
@@ -3,6 +3,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
 import java.util.List;
 
 public class UnoGUI extends JFrame {
@@ -13,12 +14,13 @@ public class UnoGUI extends JFrame {
 
     private JPanel playerHandPanel;
     private JLabel topCardLabel;
+    private JLabel currentPlayerLabel;
 
     public UnoGUI() {
         players = new ArrayList<>();
         deck = createDeck();
         shuffleDeck(deck);
-        currentPlayer = null;
+        currentPlayer = players.isEmpty() ? null : players.get(0);
 
         setTitle("Uno Game");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -34,10 +36,17 @@ public class UnoGUI extends JFrame {
     }
 
     private List<Card> createDeck() {
-        // Implement the logic to create a deck
-        // ...
+        List<Card> deck = new ArrayList<>();
 
-        return new ArrayList<>();
+        for (Card.Suit suit : Card.Suit.values()) {
+            for (Card.Rank rank : Card.Rank.values()) {
+                deck.add(new Card(rank, suit));
+            }
+        }
+
+        // Add more cards or adjust the deck creation logic based on Uno rules
+
+        return deck;
     }
 
     private void shuffleDeck(List<Card> deck) {
@@ -63,35 +72,104 @@ public class UnoGUI extends JFrame {
                 int playerCount = Integer.parseInt(playerCountField.getText());
                 initializePlayers(playerCount);
                 startGame();
+
+                playerCountField.setEnabled(false);
+                startButton.setEnabled(false);
+                playerRangePanel.setVisible(false);
             }
         });
     }
 
     private void initializePlayers(int playerCount) {
         players.clear();
+
         for (int i = 0; i < playerCount; i++) {
             players.add(new Player("Player " + (i + 1)));
         }
     }
 
     private void startGame() {
+        initializePlayers(players.size());
+
         currentPlayer = players.get(0);
         updateCardVisibility();
-        // Implement the logic to start the game
+        updateCurrentPlayerLabel();
+
+        // Distribute initial cards to players
+        for (Player player : players) {
+            for (int i = 0; i < 7; i++) {  // Distribute 7 cards to each player
+                Card drawnCard = deck.remove(0);
+                player.addToHand(drawnCard);
+            }
+        }
+
+        // Set the top card on the discard pile
+        topCard = deck.remove(0);
+
+        // Handle special cards at the beginning (if needed)
+        handleSpecialCards(topCard);
+
+        // Start the first player's turn
+        playTurn();
+    }
+
+    private void playTurn() {
+        // Implement the logic for a player's turn
         // ...
+
+        // After a turn is completed, move to the next player
+        moveToNextPlayer();
+
+        // Update card visibility for the new current player
+        updateCardVisibility();
+    }
+
+    private void moveToNextPlayer() {
+        if (currentPlayer!= null) {
+            int currentIndex = players.indexOf(currentPlayer);
+            int nextIndex = (currentIndex + 1) % players.size();
+            currentPlayer = players.get(nextIndex);
+            updateCurrentPlayerLabel();
+        }
+    }
+
+    private void handleSpecialCards(Card topCard) {
+        // Implement the logic to handle special cards at the beginning of the game
+        // For example, handle WILD cards or DRAW2 cards
+        // ...
+    }
+
+    // Example of a method to handle playing a card
+    private void playCard(Card selectedCard) {
+        // Implement the logic to handle playing a card
+        // ...
+
+        // After playing the card, check if the player has won, or continue to the next player
+        // ...
+
+        // Start the next player's turn
+        playTurn();
     }
 
     private void setupCardVisibility() {
         playerHandPanel = new JPanel();
         topCardLabel = new JLabel();
+        currentPlayerLabel = new JLabel();
+
+        JPanel southPanel = new JPanel();
+        southPanel.setLayout(new GridLayout(2, 1));
+        southPanel.add(topCardLabel);
+        southPanel.add(currentPlayerLabel);
 
         add(playerHandPanel, BorderLayout.CENTER);
-        add(topCardLabel, BorderLayout.SOUTH);
+        add(southPanel, BorderLayout.SOUTH);
     }
 
     private void updateCardVisibility() {
-        displayPlayerHand();
-        displayTopCard();
+        if (!players.isEmpty() && currentPlayer != null) {
+            displayPlayerHand();
+            displayTopCard();
+        }
     }
 
     private void displayPlayerHand() {
@@ -116,22 +194,42 @@ public class UnoGUI extends JFrame {
     }
 
     private void displayTopCard() {
-        topCardLabel.setText("Top Card: " + topCard.toString());
+        if (topCard != null) {
+            topCardLabel.setText("Top Card: " + topCard.toString());
+        } else {
+            topCardLabel.setText("Top Card: None");
+        }
     }
 
     private void setupGUIComponents() {
+        JButton viewStatusButton = new JButton("View Status");
         JButton drawCardButton = new JButton("Draw Card");
         JButton nextPlayerButton = new JButton("Next Player");
 
         JPanel buttonPanel = new JPanel();
+        buttonPanel.add(viewStatusButton);
         buttonPanel.add(drawCardButton);
         buttonPanel.add(nextPlayerButton);
 
+        JPanel southPanel = new JPanel();
+        southPanel.add(currentPlayerLabel);
+        southPanel.add(topCardLabel);
+        add(southPanel, BorderLayout.SOUTH);
+
+
         add(buttonPanel, BorderLayout.EAST);
+
+        viewStatusButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                displayPlayerHands();
+            }
+        });
 
         drawCardButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                drawCard();
                 // Implement the logic to handle draw card button click
                 // ...
             }
@@ -140,10 +238,39 @@ public class UnoGUI extends JFrame {
         nextPlayerButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                moveToNextPlayer();
+                updateCardVisibility();
                 // Implement the logic to handle next player button click
                 // ...
             }
         });
+
+        updateCurrentPlayerLabel();
+
+    }
+
+    private void drawCard() {
+        Card drawnCard = currentPlayer.drawCardFromDeck(deck);
+        if (drawnCard != null) {
+            updateCardVisibility();
+        }
+    }
+
+    private void displayPlayerHands() {
+        JTextArea playerHandsTextArea = new JTextArea();
+        playerHandsTextArea.setEditable(false);
+        playerHandsTextArea.append("Player Hands:\n");
+
+        for (Player player : players) {
+            playerHandsTextArea.append(player.getName() + "'s Hand: " + player.getHand() + "\n");
+        }
+
+        JOptionPane.showMessageDialog(this, new JScrollPane(playerHandsTextArea),
+                "Player Hands", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    private void updateCurrentPlayerLabel() {
+        currentPlayerLabel.setText("Current Player: ");
     }
 
     public static void main(String[] args) {
